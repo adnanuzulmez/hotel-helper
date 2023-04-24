1b4b4247efcc3b65770fc33776f95e2c45a571e0
Omer <adnan.uzulmez@perwatch.com>
2023-04-24 16:22:14 +0300

    Auth process Ok

---
+++ src/components/Mainpage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="mainpage">
+    <h1>ok</h1>
+  </div>
+</template>
+
+<script>
+export default {
+ data(){
+    return{
+        
+    }
+ }
+}
+</script>
+
+<style>
+
+</style>
