8e1be9a3e338c8af99d4bf951220b372a34b647a
adnanuzulmez <adnan.uzulmez@perwatch.com>
2023-06-21 19:45:36 +0300

    feat: reservation page designed and started

---
+++ src/components/Reservation.vue
@@ -0,0 +1,163 @@
+<template>
+    <div class="booking">
+    <h3 class="titles">
+        <svg  style="margin-right: 10px;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-door-closed" viewBox="0 0 16 16">
+            <path d="M3 2a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V2zm1 13h8V2H4v13z"/>
+            <path d="M9 9a1 1 0 1 0 2 0 1 1 0 0 0-2 0z"/>
+            </svg>
+    Rezervasyonlar</h3>
+        <div class="header">
+            
+            <div class="header-title">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person"
+                    viewBox="0 0 16 16">
+                    <path
+                        d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
+                </svg>
+                <div>İsim</div>
+            </div>
+            <div class="header-title">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person"
+                    viewBox="0 0 16 16">
+                    <path
+                        d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
+                </svg>
+                <div>Soyisim</div>
+            </div>
+            <div class="header-title"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+            <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"/>
+            </svg>
+                <div>Otel</div>
+            </div>
+            <div class="header-title"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-door-closed" viewBox="0 0 16 16">
+            <path d="M3 2a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V2zm1 13h8V2H4v13z"/>
+            <path d="M9 9a1 1 0 1 0 2 0 1 1 0 0 0-2 0z"/>
+            </svg>
+                <div>Oda</div>
+            </div>
+            <div class="header-title">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="bi bi-calendar-date" viewBox="0 0 16 16">
+                    <path
+                        d="M6.445 11.688V6.354h-.633A12.6 12.6 0 0 0 4.5 7.16v.695c.375-.257.969-.62 1.258-.777h.012v4.61h.675zm1.188-1.305c.047.64.594 1.406 1.703 1.406 1.258 0 2-1.066 2-2.871 0-1.934-.781-2.668-1.953-2.668-.926 0-1.797.672-1.797 1.809 0 1.16.824 1.77 1.676 1.77.746 0 1.23-.376 1.383-.79h.027c-.004 1.316-.461 2.164-1.305 2.164-.664 0-1.008-.45-1.05-.82h-.684zm2.953-2.317c0 .696-.559 1.18-1.184 1.18-.601 0-1.144-.383-1.144-1.2 0-.823.582-1.21 1.168-1.21.633 0 1.16.398 1.16 1.23z" />
+                    <path
+                        d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+                </svg>
+                <div></div>Giris Tarihi
+            </div>
+            <div class="header-title">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="bi bi-calendar-date" viewBox="0 0 16 16">
+                    <path
+                        d="M6.445 11.688V6.354h-.633A12.6 12.6 0 0 0 4.5 7.16v.695c.375-.257.969-.62 1.258-.777h.012v4.61h.675zm1.188-1.305c.047.64.594 1.406 1.703 1.406 1.258 0 2-1.066 2-2.871 0-1.934-.781-2.668-1.953-2.668-.926 0-1.797.672-1.797 1.809 0 1.16.824 1.77 1.676 1.77.746 0 1.23-.376 1.383-.79h.027c-.004 1.316-.461 2.164-1.305 2.164-.664 0-1.008-.45-1.05-.82h-.684zm2.953-2.317c0 .696-.559 1.18-1.184 1.18-.601 0-1.144-.383-1.144-1.2 0-.823.582-1.21 1.168-1.21.633 0 1.16.398 1.16 1.23z" />
+                    <path
+                        d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+                </svg>
+                <div></div>Çıkış Tarihi
+            </div>
+             <div class="header-title">
+              
+            </div> 
+        </div>
+        <div class="booking-content">
+            <div class="booking-template">
+                <div class="reservation-datas"  v-for="users, index in resDatas" :key="index">
+                    <div class="header-title">
+                        {{ users.user.name}}
+                    </div>
+                    <div class="header-title">
+                        {{  users.user.surname}}
+                    </div>
+                    <div class="header-title">
+                        {{ users.hotel.name}}
+                    </div>
+                    <div class="header-title">
+                        {{ users.room.roomNo}}
+                    </div>
+                    <div class="header-title">
+                        {{ users.checkInDate }}
+                    </div>
+                    <div class="header-title">
+
+                        {{ users.checkOutDate }}
+                    </div>
+                    <div class="header-title" style="justify-content: space-between;">
+                        
+                        <div class="buttons" style="margin-right: 10px; display: flex;">
+                            <div class="delete">
+                                <svg @click="deleteReservation(users._id, users.room._id)" style="margin-right: 8px; color: #ff8181; cursor: pointer;" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                    class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                                    <path
+                                        d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
+                                </svg>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import { Toast } from "toaster-js";
+import "toaster-js/default.css"; 
+export default {
+    name: 'Reservation',
+
+    data() {
+        return {
+            resDatas: [],
+        };
+    },
+
+    mounted() {
+        this.getReservations()
+    },
+
+    methods: {
+        getReservations(){
+            axios.get('http://192.168.1.109:3000/reservations')
+            .then((response) => {
+                this.resDatas = response.data
+            })
+        },
+        deleteReservation(id, roomId){
+            const params = {
+                id: id,
+                roomId: roomId
+            }
+            axios.delete('http://192.168.1.109:3000/reservations', {params})
+            .then((response) => {
+                console.log(params);
+                this.getReservations()
+                new Toast("Rezervasyon Silindi", Toast.TYPE_DONE, Toast.TIME_LONG);
+            })
+        }
+    },
+};
+</script>
+
+<style >
+
+.titles{
+    color: #0487c7;
+    filter: drop-shadow(0px 0px 10px #7bbdff);
+    display: flex;
+    align-items: center;
+}
+.reservation-datas {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    padding: 5px 0;
+    margin: 15px 0;
+    background-color: #d9ffeb;
+    border-radius: 5px;
+    box-shadow: rgb(93 255 174 / 24%) 0px 1px 14px;
+    border: 1px solid #aed1ae;
+}
+</style>
